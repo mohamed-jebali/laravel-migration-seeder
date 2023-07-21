70448e8584b60818df0bd16fa4ab857577f32cc9
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-07-21 16:42:06 +0200

    keep having problems with table generation

--- database/migrations/2023_07_21_143457_create_trains_table.php
+++ database/migrations/2023_07_21_143457_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company_name', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('arrival_time');
+            $table->dateTime('departure_time');
+            $table->smallInteger('code_train',20);
+            $table->tinyInteger('train_number_carriage');
+            $table->string('in_time', 50);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
